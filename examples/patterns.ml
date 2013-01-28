24ae0ac951916e0c9d816a8860bff303732b46c9
Jeremy Yallop <yallop@gmail.com>
2013-01-28 12:02:31 +0000

    Tabs -> spaces.

--- examples/patterns.ml
+++ examples/patterns.ml
@@ -8,23 +8,23 @@ end
 
 let tests () = ListMonad.(begin
   let vs = perform (`A x <-- [`A 1; `B '2'; `C true; `A 4; `A 5; `B '6'];
-		    return (x * 2)) in
+                    return (x * 2)) in
     assert (vs = [2; 8; 10]);
 
   let vs = perform (`A x <-- [`A 1; `B '2'; `C true; `A 4; `A 5; `B '6'];
-		    (y, Some z) <-- [(1, None); (2, Some 3); (4, Some 5)];
-		    return (x + y + z)) in
+                    (y, Some z) <-- [(1, None); (2, Some 3); (4, Some 5)];
+                    return (x + y + z)) in
     assert (vs = [1 + 2 + 3;
-		  1 + 4 + 5;
-		  4 + 2 + 3;
-		  4 + 4 + 5;
-		  5 + 2 + 3;
-		  5 + 4 + 5]);
+                  1 + 4 + 5;
+                  4 + 2 + 3;
+                  4 + 4 + 5;
+                  5 + 2 + 3;
+                  5 + 4 + 5]);
 
   let vs = perform ([|x; y|] <-- [[|1; 2|]; [||]; [|3; 4; 5|]; [|6; 7|]];
-		    return (x + y)) in
+                    return (x + y)) in
     assert (vs = [1 + 2;
-		  6 + 7]);
+                  6 + 7]);
 end)
 
 let () = begin
