4de50d6b9283629ef1c7e24163eb8e735add6bdc
Jeremy Yallop <yallop@gmail.com>
2013-01-28 10:07:54 +0000

    Allow patterns on the lhs of monad-notation bindings.

--- src/ppx_monad.ml
+++ src/ppx_monad.ml
@@ -39,6 +39,33 @@ open Longident
 
 let fail_exit_code = ref 0
 
+exception Pattern_translation_failure of Location.t
+
+let map_opt f = function
+    None -> None
+  | Some e -> Some (f e)
+
+let rec patt_of_expr : expression -> pattern = 
+  fun {pexp_desc; pexp_loc} ->
+    let desc = match pexp_desc with
+      | Pexp_ident {txt = Lident txt; loc} -> Ppat_var { txt; loc }
+      | Pexp_constant c -> Ppat_constant c
+      | Pexp_tuple es -> Ppat_tuple (List.map patt_of_expr es)
+      | Pexp_record (fields, basis) ->
+        Ppat_record (List.map (fun (f, e) -> f, patt_of_expr e) fields, Open)
+      | Pexp_construct (ident, e_opt, flag) ->
+        Ppat_construct (ident, map_opt patt_of_expr e_opt, flag)
+      | Pexp_variant (label, e_opt) ->
+        Ppat_variant (label, map_opt patt_of_expr e_opt)
+      | Pexp_array es -> Ppat_array (List.map patt_of_expr es)
+      | Pexp_constraint (e, Some t, None) ->
+        Ppat_constraint (patt_of_expr e, t)
+      | Pexp_lazy e -> Ppat_lazy (patt_of_expr e)
+      (* There's no expression syntax corresponding to _-, as-, or- or #-
+         patterns. *)
+      | _ -> raise (Pattern_translation_failure pexp_loc)
+    in { ppat_desc = desc; ppat_loc = pexp_loc }
+
 let mapper =
   object(this)
 
@@ -60,17 +87,16 @@ let mapper =
             next)
           when in_monad ->
 
-        begin match lhs.pexp_desc with
-        | Pexp_ident { txt = Lident nm } ->
+        let patt =
+          try patt_of_expr lhs 
+          with Pattern_translation_failure loc ->
+            Format.eprintf "%appx-monad: Invalid pattern.@." Location.print loc;
+            exit !fail_exit_code
+        in
           E.apply_nolabs (E.lid "bind")
             [ this # expr rhs;
               E.function_ "" None
-                [ P.var (Location.mkloc nm Location.none), this # expr next ] ]
-
-        | _ ->
-          Format.eprintf "%appx-monad: Expected variable binding.@." Location.print lhs.pexp_loc;
-          exit !fail_exit_code
-        end
+                [ patt, this # expr next ] ]
 
       | Pexp_sequence (first, second) when in_monad ->
         E.apply_nolabs (E.lid "bind")
